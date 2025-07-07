071bef37139e814cc87bffb4858997a802df2528
nuclearjack <55406201+nuclearjack@users.noreply.github.com>
2025-07-07 17:51:51 +0500

    Update personToCard.ts

--- src/widgets/person/mappers/personToCard.ts
+++ src/widgets/person/mappers/personToCard.ts
@@ -5,6 +5,6 @@ export const personToCard = (person: Person) => {
   return {
     ...person,
     birth_date: person.birth_date ? formatDate(person.birth_date) : 'Нет данных',
-    death_date: person.death_date ? formatDate(person.death_date) : 'наши дни'
+    death_date: person.death_date ? formatDate(person.death_date) : ''
   }
 }
